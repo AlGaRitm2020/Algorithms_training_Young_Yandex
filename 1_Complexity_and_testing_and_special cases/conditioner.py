b065a2104dc2fe758853037e913425d1ebbb8605
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-06-18 08:25:29 +0400

    solved conditioner

--- 1_Complexity_and_testing_and_special cases/conditioner.py
+++ 1_Complexity_and_testing_and_special cases/conditioner.py
@@ -1,1 +1,16 @@
-print('initial commit')
+now, goal = (int(i) for i in input().split())
+mode = input()
+if mode == 'fan':
+    print(now)
+elif mode == 'auto':
+    print(goal)
+elif mode == 'freeze':
+    if now > goal:
+        print(goal)
+    else:
+        print(now)
+else:
+    if now < goal:
+        print(goal)
+    else:
+        print(now)
